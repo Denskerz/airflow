ea24fcdb25a8349aff8c2d1acd5304e7cc1eab9d
Denskerz <de.errr18@gmail.com>
2024-08-28 04:20:46 -0700

    Update dag_clear_logs.py

--- dag_clear_logs.py
+++ dag_clear_logs.py
@@ -14,9 +14,7 @@ import warnings
 warnings.filterwarnings("ignore")
 
 date_today = dt.datetime.today()
-date_now = dt.datetime.now().strftime("%Y_%m_%d %H:%m")
-dag_id = "dq_check_curs"
-file_codename = f'\'{dt.datetime.today().strftime("%Y-%m-%d")}''
+dag_id = "clear_logs"
 email_notification_list_failure = [
     "eroshevichdv@sber-bank.by",
 ]
@@ -27,7 +25,7 @@ email_notification_list_success = [
 
 args = {
     "owner": "BICC",
-    "start_date": dt.datetime(2023, 8, 21),
+    "start_date": dt.datetime(2024, 8, 25),
     "retries": 1,
     "retry_delay": dt.timedelta(minutes=2),
     "depends_on_past": False,
@@ -35,27 +33,25 @@ args = {
     "email_on_failure": True,
 }
 
-def filter_content(folders)
-    folders = folders.replace("latest,", "")
-    folders_list = folders.split(',')
+
+def filter_content(**kwargs):
+    folders = kwargs['ti'].xcom_pull(task_ids='check_logs_content')
+    folders_list = folders.replace(",latest,", "").split(',')
     unnecessary_folders = ""
     for item in folders_list:
         item_obj = item.strptime(item, "Y-%m-%d")
         if item_obj < date_today - datetime.timedelta(days=10):
-             unnecessaru_folders = unnecessary_folders + " "  + item
-    ti.xcom_push(key='unnessary_folders', value=unnecessary_folders
-
+            unnecessary_folders = unnecessary_folders + " " + item
+    kwargs['ti'].xcom_push(key='unnecessary_folders', value=unnecessary_folders)
 
 
 with DAG(
-    dag_id=dag_id, default_args=args, schedule_interval="30 15 * * *"
+        dag_id=dag_id, default_args=args, schedule_interval="30 15 * * *"
 ) as dag:  # run daily at 15:30
-
 
     check_logs_content = BashOperator(
         task_id="check_logs_content",
-        bash_command="ls -1 /home/airflow/airflow_services/logs/scheduler | tr '\n', ','" ,
-        xcom_push=True,
+        bash_command="ls -1 /home/airflow/airflow_services/logs/scheduler | tr '\n', ','",
         do_xcom_push=True,
         dag=dag,
     )
@@ -69,9 +65,9 @@ with DAG(
 
     clear_logs = BashOperator(
         task_id='clear_logs',
-        bash_command="rm -rf {{ ti.xcom_pull(task_ids='filter_content', key='unnecessary_folders') }} ",
+        bash_command='rm -rf "{{ ti.xcom_pull(task_ids=\'filter_content\', key=\'unnecessary_folders\') }} "',
         depends_on_past=True,
-	dag=dag,
+        dag=dag,
     )
 
-    check_logs_content >> filter_content >> clear_logs
+check_logs_content >> filter_content >> clear_logs
